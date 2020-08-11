aceb1d7e2ed2f98b7224d9883f4883d6c073686a
Raziel Tabib <raziel@raziels-mbp.attlocal.net>
2020-08-10 23:25:40 -0700

    changes to rollout - not stable yet

--- kyml.go
+++ kyml.go
@@ -5,12 +5,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"gopkg.in/yaml.v2"
+	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
+	"github.com/ghodss/yaml"
 	"io/ioutil"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 
@@ -52,23 +58,31 @@ func main() {
 func update(txtContext, txtKind,txtName,txtAtt,txtVal string, intIndex int) error {
 
 	err := filepath.Walk(txtContext, func(path string, info os.FileInfo, err error) error {
+		//fmt.Println(path)
+
 		if info.IsDir() {
 			return nil
 		}
-		matched, _ := regexp.MatchString(`\.yaml$`, path)
+		matched, err := regexp.MatchString(`\.yaml$`, path)
+		if err != nil {
+			log.Fatalf("MatchString: %v", err)
+			return err
+		}
+
 		if matched{
 			yamlFile, err := ioutil.ReadFile(path)
 			if err != nil {
 				log.Printf("yamlFile.Get err   #%v ", err)
 			}
-			if txtKind == "deployment"{
-				var deployment Deployment
-				err = yaml.Unmarshal([]byte(yamlFile), &deployment)
-				if err != nil {
-					log.Fatalf("Unmarshal: %v", err)
-				}
-				if deployment.Meta.Name == txtName{
-
+			matched, err = matchResource(txtKind,txtName,[]byte(yamlFile))
+			if matched{
+				switch txtKind{
+				case "deployment":
+					var deployment Deployment
+					err = yaml.Unmarshal([]byte(yamlFile), &deployment)
+					if err != nil {
+						log.Fatalf("Unmarshal: %v", err)
+					}
 					if txtAtt == "image"{
 						fmt.Printf("Updating resource of kind: %s\tNamed: %s\tImage:%s ==> %s\n",deployment.Kind,deployment.Meta.Name,deployment.Spec.Template.Spec.Containers[intIndex].Image,txtVal)
 						deployment.Spec.Template.Spec.Containers[intIndex].Image = txtVal
@@ -81,17 +95,29 @@ func update(txtContext, txtKind,txtName,txtAtt,txtVal string, intIndex int) erro
 							log.Fatal(err)
 						}
 					}
-				}
-
-			}
-			if txtKind == "rollout"{
-				var rollout Rollout
-				err = yaml.Unmarshal([]byte(yamlFile), &rollout)
-				if err != nil {
-					log.Fatalf("Unmarshal: %v", err)
-				}
-				if rollout.Meta.Name == txtName{
-
+				case "rollout":
+					var ro v1alpha1.Rollout
+					err = yaml.UnmarshalStrict([]byte(yamlFile), &ro, yaml.DisallowUnknownFields)
+					if err != nil {
+						panic(err)
+					}
+					if txtAtt == "image"{
+						fmt.Printf("Updating resource of kind: %s\tNamed: %s\tImage:%s ==> %s\n",ro.Kind,ro.GetName(),ro.Spec.Template.Spec.Containers[intIndex].Image,txtVal)
+						ro.Spec.Template.Spec.Containers[intIndex].Image = txtVal
+						data, err := yaml.Marshal(&ro)
+						if err != nil {
+							log.Fatalf("error: %v", err)
+						}
+						err = ioutil.WriteFile(path, data, 0644)
+						if err != nil {
+							log.Fatal(err)
+						}
+					}
+					/*var rollout Rollout
+					err = yaml.Unmarshal([]byte(yamlFile), &rollout)
+					if err != nil {
+						log.Fatalf("Unmarshal: %v", err)
+					}
 					if txtAtt == "image"{
 						fmt.Printf("Updating resource of kind: %s\tNamed: %s\tImage:%s ==> %s\n",rollout.Kind,rollout.Meta.Name,rollout.Spec.Template.Spec.Containers[intIndex].Image,txtVal)
 						rollout.Spec.Template.Spec.Containers[intIndex].Image = txtVal
@@ -103,13 +129,13 @@ func update(txtContext, txtKind,txtName,txtAtt,txtVal string, intIndex int) erro
 						if err != nil {
 							log.Fatal(err)
 						}
-					}
+					}*/
+				default:
+					fmt.Printf("Kind %s is not supported yet\n",txtKind)
+					os.Exit(1)
 				}
-
 			}
-
 		}
-
 		return nil
 	})
 	if err != nil {
@@ -119,3 +145,14 @@ func update(txtContext, txtKind,txtName,txtAtt,txtVal string, intIndex int) erro
 
 	}
 
+func matchResource (txtKind, txtName string, data []byte ) (bool, error) {
+	var base BaseInfo
+	err := yaml.Unmarshal(data, &base)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	if strings.EqualFold(base.Kind,txtKind) && strings.EqualFold(base.Meta.Name,txtName) {
+		return true, nil
+	}
+	return false, nil
+}
--- schema.go
+++ schema.go
@@ -1,5 +1,15 @@
 package main
 
+import "k8s.io/apimachinery/pkg/util/intstr"
+
+type BaseInfo struct {
+	ApiVersion 		string `yaml:"apiVersion"`
+	Kind			string `yaml:"kind"`
+	Meta 			struct{
+		Name 		string `yaml:"name"`
+	} `yaml:"metadata"`
+}
+
 type Deployment struct {
 	ApiVersion 		string `yaml:"apiVersion"`
 	Kind			string `yaml:"kind"`
@@ -7,13 +17,26 @@ type Deployment struct {
 		Name 		string `yaml:"name"`
 	} `yaml:"metadata"`
 	Spec			struct{
+		Replicas 				int	`yaml:"replicas"`
+		RevisionHistoryLimit	int	`yaml:"revisionHistoryLimit"`
+		Selector 	struct{
+			MatchLabels map[string]string `yaml:"matchLabels"`
+		}`yaml:"selector"`
 		Template 	struct{
+			Metadata struct{
+				Labels map[string]string `yaml:"labels"`
+			}`yaml:"metadata"`
 			Spec	struct{
 				Containers []struct{
-					Image			string `yaml:"image"`
+					Image		string `yaml:"image"`
+					Name 		string `yaml:"name"`
+					Ports 		[]struct{
+						ContainerPort 		int	`yaml:"containerPort"`
+					} `yaml:"ports"`
 				}`yaml:"containers"`
 			} `yaml:"spec"`
 		} `yaml:"template"`
+		MinReadySeconds		int `yaml:"minReadySeconds"`
 	} `yaml:"spec"`
 }
 
@@ -24,12 +47,45 @@ type Rollout struct {
 		Name 		string `yaml:"name"`
 	} `yaml:"metadata"`
 	Spec			struct{
+		Replicas 				int	`yaml:"replicas"`
+		RevisionHistoryLimit	int	`yaml:"revisionHistoryLimit"`
+		Selector 	struct{
+			MatchLabels map[string]string `yaml:"matchLabels"`
+		}`yaml:"selector"`
 		Template 	struct{
+			Metadata struct{
+				Labels map[string]string `yaml:"labels"`
+			}`yaml:"metadata"`
 			Spec	struct{
 				Containers []struct{
-					Image			string `yaml:"image"`
+					Image		string `yaml:"image"`
+					Name 		string `yaml:"name"`
+					Ports 		[]struct{
+						ContainerPort 		int	`yaml:"containerPort"`
+					} `yaml:"ports"`
 				}`yaml:"containers"`
 			} `yaml:"spec"`
 		} `yaml:"template"`
+		MinReadySeconds		int `yaml:"minReadySeconds,omitempty"`
+		Strategy 		struct{
+			Canary		struct{
+				Steps []map[string]string `json:"steps,omitempty"`
+			}`yaml:"canary,omitempty"`
+		}`yaml:"strategy"`
 	} `yaml:"spec"`
+}
+
+type CanaryStep struct {
+	// SetWeight sets what percentage of the newRS should receive
+	SetWeight *int32 `json:"setWeight,omitempty"`
+	// Pause freezes the rollout by setting spec.Paused to true.
+	// A Rollout will resume when spec.Paused is reset to false.
+	// +optional
+	Pause RolloutPause `json:"pause,omitempty"`
+}
+
+type RolloutPause struct {
+	// Duration the amount of time to wait before moving to the next step.
+	// +optional
+	Duration *intstr.IntOrString `json:"duration,omitempty"`
 }
